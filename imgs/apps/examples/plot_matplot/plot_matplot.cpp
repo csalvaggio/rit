eea89849b39ba34d11aa72efdc709889ebbc981f
Carl Salvaggio <salvaggio@cis.rit.edu>
2021-03-02 10:34:47 -0500

    Updated matplotplusplus example app

--- imgs/apps/examples/plot_matplot/plot_matplot.cpp
+++ imgs/apps/examples/plot_matplot/plot_matplot.cpp
@@ -9,7 +9,15 @@ int main() {
   std::vector<double> x = {0, 1, 2, 3, 4, 5, 6};
   std::vector<double> y = {0, 1, 4, 9, 16, 25, 36};
 
-  matplot::plot(x, y);
+  matplot::plot(x, y, "-s")
+      ->color("r")
+      .line_width(1)
+      .marker_size(10)
+      .marker_color("b")
+      .marker_face_color({0.0, 1.0, 1.0});
+  matplot::xlabel("x");
+  matplot::ylabel("y");
+
   matplot::show();
 
   return EXIT_SUCCESS;
